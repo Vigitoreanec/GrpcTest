c122913dd976138026756d6efb14bd5d0cc59ec2
Vigitoreanec <chirik39@mail.ru>
2024-12-05 12:05:42 +0200

    Добавьте файлы проекта.

---
+++ GrpcTest/Services/GreeterService.cs
@@ -0,0 +1,16 @@
+using Grpc.Core;
+using GrpcTest;
+
+namespace GrpcTest.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
